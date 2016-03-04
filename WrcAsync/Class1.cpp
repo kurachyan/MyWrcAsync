1af90a0c47d55c7c1c17fa25f557b30709369b83
kurachyan <e_tamakou@outlook.jp>
2016-03-05 00:05:36 +0900

    初期設定１

---
+++ WrcAsync/Class1.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace WrcAsync;
+using namespace Platform;
+
+Class1::Class1()
+{	// コンストラクタ
+}
+
+//Class1::~Class1()
+//{	// ディストラクタ
+//}
+
+int Class1::add(int a, int b)
+{	// Add
+	return(a + b);
+}
+
+int Class1::sub(int a, int b)
+{	// Sub
+	return(a - b);
+}
+
+int Class1::mul(int a, int b)
+{	// Mul
+	return(a * b);
+}
+
+int Class1::div(int a, int b)
+{	// Div
+	if (b != 0)
+	{
+		return(a / b);
+	}
+	else
+	{
+		return(0);		// Div by Zero
+	}
+}
---
+++ WrcAsync/Class1.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+namespace WrcAsync
+{
+    public ref class Class1 sealed
+    {
+    public:
+		Class1();		// コンストラクタ
+//		~Class1();		// ディストラクタ
+
+		int add(int a, int b);		// Add
+		int sub(int a, int b);		// Sub
+		int mul(int a, int b);		// Mul
+		int div(int a, int b);		// Div
+    };
+}
